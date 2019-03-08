fba578ad912f79f2f61757826a0605667b340daf
mukundzalke <mukundzalke@gamil.com>
2019-03-08 19:03:01 +0530

    Architech American Guy Commit3

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -14,7 +14,25 @@ public class RESTAPITest {
 	}
 	
 	@Test
+	public void devlopFrameworkforJira()
+	{
+		System.out.println("postJira");
+		//CHANGES IN INDIAN TIME
+		System.out.println("postJira");
+		System.out.println("postJira");
+
+	}
+	
+	@Test
 	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");	
+		System.out.println("deleteTwitter");	
+
+	}
+	
+	@Test
+	public void devlopFrameworkforTwitter()
 	{
 		System.out.println("deleteTwitter");	
 		System.out.println("deleteTwitter");	
